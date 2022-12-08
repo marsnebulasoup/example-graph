cbe86315b925e72022226d1bf022a33aefc34db3
marsnebulasoup <52974834+marsnebulasoup@users.noreply.github.com>
2022-12-08 16:00:55 -0600

    Implemented findVertex

--- graph.cpp
+++ graph.cpp
@@ -10,3 +10,10 @@ bool Graph::isValid(const int id, const string *data)
 {
   return id > 0 && data && data->length();
 }
+
+Vertices *Graph::findVertex(int id)
+{
+  Vertices search = Vertices{Vertex{id}};
+  Vertices *vertices = *list.find(&search);
+  return vertices == *list.end() ? NULL : vertices;
+}
