edcdef7fb0cec027a8013bd1f3cc69195a04f97c
marsnebulasoup <52974834+marsnebulasoup@users.noreply.github.com>
2022-12-08 16:00:05 -0600

    Added graph implemtation

---
+++ graph.h
@@ -0,0 +1,45 @@
+/*********************
+Name: Kevin Bryniak
+Assignment: Final Project
+An implementation of a graph class
+**********************/
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <set>
+#include <vector>
+#include <string>
+#include "vertex.h"
+
+using std::set;
+using std::string;
+using std::to_string;
+using std::vector;
+
+class Graph
+{
+private:
+  set<Vertices *, vertices_cmp> list;
+
+  bool isValid(const int, const string *);
+  Vertices *findVertex(int);
+
+public:
+  bool addVertex(const int, const string *);
+  bool addEdge(int, int);
+  bool removeVertex(int);
+  bool removeEdge(int, int);
+  bool isEmpty();
+  bool getVertex(int, Vertex *);
+  bool getEdges(int, vector<Vertex *> *);
+  bool containsVertex(int);
+  bool containsEdge(int, int);
+  int getVertexCount();
+  int getEdgeCount(int);
+  int getTotalEdgeCount();
+
+  void dumpGraph(string &);
+};
+
+#endif
